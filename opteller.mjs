3caaec7d2ded4b9ebf5fa0251bc4bfdc7f1e417c
MatteoDia <matteo.diaz.niembro@student.ehb.be>
2024-09-24 10:31:12 +0100

    optellerm2
    opteller met waarde geven aan variabel

--- opteller.mjs
+++ opteller.mjs
@@ -3,13 +3,16 @@ import{stdin as input, stdout as output} from 'node:process';
 const userInput = readline.createInterface({input, output});
 
 
-let getal1= parseFloat(await userInput.question("geef eerste getal: "));
+let getal1= 56
 
-let getal2 = parseFloat(await userInput.question("geef tweede getal: "));
+let getal2 = "34"
 
 let som = getal1 + getal2;
+let deling= getal1/getal2;
+let verm= getal1 * getal2;
 
-console.log("de som is = " + som);
+
+console.log("De som is = " + som + ". De deling is: " + deling + ".  Het product is: " + verm);
 process.exit()
 
 
